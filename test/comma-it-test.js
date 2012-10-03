cf8efa02663be9c47230f156146ac282599ade70
AvnerCohen <israbirding@gmail.com>
2012-10-03 18:41:25 +0200

    Changed to accomdated for the default npm website use case

--- test/comma-it-test.js
+++ test/comma-it-test.js
@@ -2,15 +2,30 @@ var commaIt = require("../comma-it").commaIt;
 var assert = require("assert");
 
 
+//## Without precision
 //Check simplest formatting
 var number = "1234";
-assert.equal(commaIt(number), "1,234.00");
+assert.equal(commaIt(number), "1,234");
 
 
 //Check precision support
 var number1 = "1234.1234";
-assert.equal(commaIt(number1), "1,234.12");
+assert.equal(commaIt(number1), "1,234");
 
 //Check multi commas
 var number2 = "12341233.12";
-assert.equal(commaIt(number2), "12,341,233.12");
+assert.equal(commaIt(number2), "12,341,233");
+
+//### With Precision support
+//Check simplest formatting
+var number = "1234";
+assert.equal(commaIt(number, true), "1,234.00");
+
+
+//Check precision support
+var number1 = "1234.1234";
+assert.equal(commaIt(number1, true), "1,234.12");
+
+//Check multi commas
+var number2 = "12341233.12";
+assert.equal(commaIt(number2, true), "12,341,233.12");

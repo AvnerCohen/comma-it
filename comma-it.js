5d37527e67081e09eb1fcc73ab614316fa66fbbc
AvnerCohen <israbirding@gmail.com>
2012-11-14 08:40:41 +0200

    Some basic formatting and travis support

--- comma-it.js
+++ comma-it.js
@@ -1,28 +1,22 @@
-/*
-Simplest possible solution to turn numbers into nicely comma seperated amounts:
-1234  => 1 234
-
-When addPrecision is set to false (or default is used = false), precision is stripped
-
-*/
+// Simplest possible solution to turn numbers into nicely  seperated amounts:
+// 1234  => 1 234
+// When addPrecision is set to false (or default is used = false), precision is stripped
 
 function commaIt(number, options) {
     //Set up default seperators
-	addPrecision = (options && options["addPrecision"] || false);
-	thousandSeperator = (options && options["thousandSeperator"] || " ");
-	decimalSeperator = (options && options["decimalSeperator"] || ",");
-    var replacmentRegex ='$1' + thousandSeperator;
+    addPrecision = (options && options['addPrecision'] || false);
+    thousandSeperator = (options && options['thousandSeperator'] || ' ');
+    decimalSeperator = (options && options['decimalSeperator'] || ',');
+    var replacmentRegex = '$1' + thousandSeperator;
 
     //Conversion to string and default return managment
-	number = number.toString();
-	if (number.length === 0) return "0" + decimalSeperator + "00";
+    number = number.toString();
+    if(number.length === 0) return '0' + decimalSeperator + '00';
 
     //Actual parsing of two side of the number
     var amount = number.split(decimalSeperator)[0];
-    var floats = addPrecision ? (decimalSeperator + ((number.split(decimalSeperator)[1] || '') +'00').substr(0,2)) : "";
-    var numberified = amount.split('').reverse().join('')
-        .replace(/(\d{3}(?!$))/g, replacmentRegex)
-        .split('').reverse().join('');
+    var floats = addPrecision ? (decimalSeperator + ((number.split(decimalSeperator)[1] || '') + '00').substr(0, 2)) : '';
+    var numberified = amount.split('').reverse().join('').replace(/(\d{3}(?!$))/g, replacmentRegex).split('').reverse().join('');
 
     return numberified + floats;
 }
--- test/comma-it-test.js
+++ test/comma-it-test.js
@@ -1,40 +1,44 @@
-var commaIt = require("../comma-it").commaIt;
-var assert = require("assert");
+var commaIt = require('../comma-it').commaIt;
+var assert = require('assert');
 
 
-var zero = "0";
-assert.equal(commaIt(zero), "0");
+var zero = '0';
+assert.equal(commaIt(zero), '0');
 
 //## Without precision
 //Check simplest formatting
-var number = "1234";
-assert.equal(commaIt(number), "1 234");
+var number = '1234';
+assert.equal(commaIt(number), '1 234');
 
 
 //Check precision support
-var number1 = "1234,1234";
-assert.equal(commaIt(number1), "1 234");
+var number1 = '1234,1234';
+assert.equal(commaIt(number1), '1 234');
 
 //Check multi commas
-var number2 = "12341233,12";
-assert.equal(commaIt(number2), "12 341 233");
+var number2 = '12341233,12';
+assert.equal(commaIt(number2), '12 341 233');
 
 //### With Precision support
 //Check simplest formatting
-var number = "1234";
-assert.equal(commaIt(number, {addPrecision:true}), "1 234,00");
+var number = '1234';
+assert.equal(commaIt(number, {addPrecision:true}), '1 234,00');
 
 
 //Check precision support
-var number1 = "1234,1234";
-assert.equal(commaIt(number1, {addPrecision:true}), "1 234,12");
+var number1 = '1234,1234';
+assert.equal(commaIt(number1, {addPrecision:true}), '1 234,12');
 
 //Check multi commas
-var number2 = "12341233,12";
-assert.equal(commaIt(number2, {addPrecision:true}), "12 341 233,12");
+var number2 = '12341233,12';
+assert.equal(commaIt(number2, {addPrecision:true}), '12 341 233,12');
 
 
 //## Checking non default seperators
 //Check > 10000 (multi thousdand seperator)
-var number2 = "12341233.12";
-assert.equal(commaIt(number2, {addPrecision:true, thousandSeperator : ",", decimalSeperator : "."}), "12,341,233.12");
+var number2 = '12341233.12';
+assert.equal(commaIt(number2, {addPrecision:true, thousandSeperator : ',', decimalSeperator : '.'}), '12,341,233.12');
+
+
+console.log('testing done');
+
